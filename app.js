77676dc630274aea8d423cd49038e7db61a49d69
jess <jvpi189@gmail.com>
2022-08-04 00:12:48 -0400

    file commit

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
 const port = 3000
 const path = require('path')
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, `${file.originalname} - ${uniqueSuffix}`)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/file', upload.single('file'), (req, res) => {
+  res.redirect('/carta')
+})
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'))
 })
@@ -9,6 +27,7 @@ app.get('/', (req, res) => {
 app.get('/carta', (req, res) => {
   res.sendFile(path.join(__dirname,'indexCarta.html'))
 })
+
 app.get('/reservacion', (req, res) => {
   res.sendFile(path.join(__dirname,'calendario.html'))
 })

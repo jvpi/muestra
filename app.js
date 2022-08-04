341a02ebd40247874dd0dc643dc2f212d8088865
jess <jvpi189@gmail.com>
2022-08-04 00:37:32 -0400

    file2 commit

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const upload = multer({ storage: storage })
 app.post('/file', /*upload.single('file'),*/ (req, res) => {
   res.redirect('/carta')
 })
+app.get('/carta', /*upload.single('file'),*/ (req, res) => {
+  res.redirect('/reservacion')
+})
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'))
 })

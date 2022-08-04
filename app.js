4e1e76c06286bc7cf32147e82ea385aa13a8df3e
jess <jvpi189@gmail.com>
2022-08-04 01:39:25 -0400

    file2 commit

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const storage = multer.diskStorage({
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, `${file.originalname} - ${uniqueSuffix}`)
+    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    //- ${uniqueSuffix}
+    cb(null, `${file.originalname}`)
   }
 })
 
